1d50557eb1c0f0dfe857713532872a0358524160
cheolhoon <power3xz@naver.com>
2017-03-03 13:04:55 +0900

    change server/db.js

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 import moment from 'moment';
 
-const DATE_FORMAT = '';
+const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 // 임시 구현
 export function insertQuestion(question) {
   let copy = Object.assign({}, question);
   copy.id = ~~(Math.random() * 100);
-  copy.createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  copy.createdAt = moment().format(DATE_FORMAT);
   copy.updatedAt = copy.createdAt;
   return new Promise((resolve, reject) => {
     resolve(copy);
