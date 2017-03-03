a18e0df74ea6df34b21d63e0c08ff57064a3708c
cheolhoon <power3xz@naver.com>
2017-03-03 15:37:59 +0900

    rename server/validator.js as server/validationMiddleware.js

--- server/validationMiddleware.js
+++ server/validationMiddleware.js
@@ -3,35 +3,10 @@ import validator from 'validator';
 // POST /qna
 export function createQuestion(req, res, next) {
   const body = req.body;
-  const titleLength = {
-    min: 10,
-    max: 50
-  };
 
-  const bodyLength = {
-    min: 20,
-    max: 400
-  };
-
-  const answerBodyLength = {
-    min: 20,
-    max: 2000
-  };
-
-  if (!validator.isLength(body.title, titleLength) ||
-    !validator.isLength(body.body, bodyLength)) {
+  if (!isValidQuestion(body)) {
     res.status(400).send('question payload validation 실패');
     return;
-  }
-
-  const answers = body.answers;
-
-  for (const answer of answers) {
-    if (validator.isEmpty(answer.lawyer) ||
-      !validator.isLength(answer.body, answerBodyLength)) {
-      res.status(400).send('question payload validation 실패');
-      return;
-    }
   }
 
   next();
@@ -57,11 +32,56 @@ export function getQuestionList(req, res, next) {
 // GET /qna/:id
 export function getQuestion(req, res, next) {
   const id = req.params.id;
-  
+
   if (!validator.isNumeric(id)) {
     res.status(400).send('path parameter validation 실패');
     return;
   }
 
   next();
+}
+
+// PUT /qna/:id
+export function updateQuestion(req, res, next) {
+  const body = req.body;
+  
+  if (!isValidQuestion(body)) {
+    res.status(400).send('question payload validation 실패');
+    return;
+  }
+
+  next();
+}
+
+function isValidQuestion(question) {
+  const titleLength = {
+    min: 10,
+    max: 50
+  };
+
+  const bodyLength = {
+    min: 20,
+    max: 400
+  };
+
+  const answerBodyLength = {
+    min: 20,
+    max: 2000
+  };
+
+  if (!validator.isLength(question.title, titleLength) ||
+    !validator.isLength(question.body, bodyLength)) {
+    return false;
+  }
+
+  const answers = question.answers;
+
+  for (const answer of answers) {
+    if (validator.isEmpty(answer.lawyer) ||
+      !validator.isLength(answer.body, answerBodyLength)) {
+      return false;
+    }
+  }
+
+  return true;
 }
